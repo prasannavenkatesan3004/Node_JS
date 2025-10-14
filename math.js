a7175ab9c312a116634a3faf8bba430bd190deee
prasannavenkatesan3004 <67313795+prasannavenkatesan3004@users.noreply.github.com>
2025-10-14 19:31:14 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,7 @@
+// math.js
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+module.exports = { add, subtract, multiply, divide };
